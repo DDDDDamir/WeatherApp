d555bb85cce1ca5cdd53fb549ce32d5ac1ee03fc
DDDDDamir <damikgg228@gmail.com>
2024-04-11 15:37:57 +0500

    2 day

--- app/src/main/java/com/example/weatherappcopy2/MainActivity.kt
+++ app/src/main/java/com/example/weatherappcopy2/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppCopy2Theme {
-                MainScreen()
             }
         }
     }
